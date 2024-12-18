61b0d3a3796aa5949ce959353eae221765e19202
GuruPrasadAnanthaneni <guruprasad.ananthaneni@gmail.com>
2024-12-18 08:53:07 +0530

    Final

--- src/Constant1.js
+++ src/Constant1.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL1 = "https://cyberbullyin-backend1.onrender.com";
+// export const BACKEND_URL1 = "https://cyberbullyin-backend1.onrender.com";
+export const BACKEND_URL="http://localhost:5501";
